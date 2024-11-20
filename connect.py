7a407d629d35516667d2b914d83a110f6f333ea0
Jesus Cevallos <jesusfcevallos@gmail.com>
2024-11-20 21:06:58 +0000

    init commit

---
+++ connect.py
@@ -0,0 +1,11 @@
+import wandb
+import importlib
+
+
+# Login to wandb
+wandb.login()
+
+def reload_modules(list_of_modules):
+    for module in list_of_modules:
+        print(f'reloading module :{module}')
+        importlib.reload(module)
